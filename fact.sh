a2c4b36fa0684717a4ddd23058c9c9abcb2f98d5
Pranav Sastry <pranavsastry@Pranavs-MacBook-Pro.local>
2021-10-25 20:58:04 +0530

    Finally got the recursive function right.. Vamos let's go factorial

--- fact.sh
+++ fact.sh
@@ -1,14 +1,18 @@
-function fact(){
+function fact {
     if (( $1==1 ))
     then
     return 1
     else
     prev=$(( $1-1 ))
-    return $(bc >>> "$1 * fact $prev")
+    fact $prev
+    prev_fact=$?
+    this_fact=$(( $1 * $prev_fact ))
+    return $this_fact
     fi
 }
 
 echo -n "Enter the number: "
 read num
-echo $(fact $num)
+fact $num
+val=$?
 echo "Factorial of "$num" is "$val""
