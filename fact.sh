5ff3cf790ca4c6230f8f17ea3c39662e14d73a7a
Pranav Sastry <pranavsastry@Pranavs-MacBook-Pro.local>
2021-10-26 10:11:46 +0530

    Fixed recursive fact. Return codes in bash are omnly from 0-255 do not use it like regular returns in python

--- fact.sh
+++ fact.sh
@@ -1,18 +1,16 @@
 function fact {
     if (( $1==1 ))
     then
-    return 1
+    echo "1"
     else
     prev=$(( $1-1 ))
-    fact $prev
-    prev_fact=$?
+    prev_fact=$(fact "$prev")
     this_fact=$(( $1 * $prev_fact ))
-    return $this_fact
+    echo $this_fact
     fi
 }
 
 echo -n "Enter the number: "
 read num
-fact $num
-val=$?
+val=$(fact "$num")
 echo "Factorial of "$num" is "$val""
