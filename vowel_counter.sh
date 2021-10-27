8a0029b172ef8ce82482b81040f10853c98ecb1d
Pranav Sastry <pranavsastry@Pranavs-MacBook-Pro.local>
2021-10-27 21:04:46 +0530

    Finally finished the vowel counter

--- vowel_counter.sh
+++ vowel_counter.sh
@@ -1,16 +1,14 @@
 echo -n "Enter the string: "
 read str
 
-vowels=(a e i o u)
 count=0
 
-for (( i=0;i<${#str[]};i++ ))
+for (( i=0;i<${#str};i++ ))
 do
-    echo ${str[$i]}
-    # if [[ ${str[$i]} in $vowels ]]
-    # then
-    #     count=$count+1
-    # fi
+    curr_char=${str:$i:1}
+    case $curr_char in
+        a|e|i|o|u) count=$(( $count + 1)) ;;
+    esac
 done
 
 echo "Number of vowels in the string is "$count""
